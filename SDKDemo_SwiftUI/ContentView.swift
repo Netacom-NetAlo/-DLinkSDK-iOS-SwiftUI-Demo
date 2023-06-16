04fac1c105182ccdb2394e71af8c2933485b8477
sondh <sondh@neta.com>
2023-06-16 16:51:20 +0700

    [imp] didReceive message

--- SDKDemo_SwiftUI/ContentView.swift
+++ SDKDemo_SwiftUI/ContentView.swift
@@ -17,10 +17,6 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Button("Show Demo") {
-                self.delegate.netAloSDK.showVNDemoVC()
-            }
-            Spacer()
             Button("Show Listgroup") {
                 self.delegate.netAloSDK.showListGroup { error in
                     let err = error as? NAError
@@ -39,10 +35,26 @@ struct ContentView: View {
                 let testContact = NAContact(id: 4785074606744072, phone: "rooney", fullName: "rooney", profileUrl: "")
                 self.delegate.netAloSDK.showCall(with: testContact, isVideoCall: false, completion: { error in
                     let err = error as? NAError
-                    print("showCall with1 err: \(err?.description ?? "")")
+                    print("showCall with err: \(err?.description ?? "")")
                 })
             }
-
+            Spacer()
+            Button("DidReceive message with json") {
+                // Conveter data "payload" in noti to json.
+                self.delegate.netAloSDK.didReceive(
+                    messageJson: "{\"messageId\":\"637844810459254798\",\"groupId\":\"4794535825664490\",\"message\":\"1234a98\",\"senderUin\":\"4785074617643083\",\"createdAt\":\"1686906959572377\",\"recipientUins\":[\"4785074617681017\",\"4785074617709101\"],\"type\":\"text\",\"attachments\":\"\",\"groupType\":\"GROUP_TYPE_PUBLIC\",\"nonce\":\"n637844810459254798\"}",
+                    completion: { error in
+                        let err = error as? NAError
+                        print("didReceive with err: \(err?.description ?? "")")
+                    }
+                )
+            }
+            Spacer()
+            Button("get numberBadge") {
+                self.delegate.netAloSDK.getNumberOfBadge({ numberOfBadge in
+                    print("numberOfBadge: \(numberOfBadge)")
+                })
+            }
         }
         .padding()
     }
